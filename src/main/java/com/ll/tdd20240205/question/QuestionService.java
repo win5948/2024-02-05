f439655d00612f347f4f725b8bea6c0401e77187
win5948 <win5948@gmail.com>
2024-02-05 16:43:42 +0900

    Qservice

---
+++ src/main/java/com/ll/tdd20240205/question/QuestionService.java
@@ -0,0 +1,17 @@
+package com.ll.tdd20240205.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
