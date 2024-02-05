4311bacc0bd71826fd6069065ae4c452ecd265d4
win5948 <win5948@gmail.com>
2024-02-05 14:46:04 +0900

    index2

--- src/main/java/com/ll/tdd20240205/MainController.java
+++ src/main/java/com/ll/tdd20240205/MainController.java
@@ -2,11 +2,14 @@ package com.ll.tdd20240205;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
     @GetMapping("/sbb")
-    public void index() {
-        System.out.println("index");
+    @ResponseBody
+    public String index() {
+        return "index";
     }
 }
