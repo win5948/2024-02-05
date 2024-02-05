662ee141121577037f81ff576693868d3db1f5dc
win5948 <win5948@gmail.com>
2024-02-05 16:46:10 +0900

    Qdetail1

--- src/main/java/com/ll/tdd20240205/question/QuestionController.java
+++ src/main/java/com/ll/tdd20240205/question/QuestionController.java
@@ -2,8 +2,9 @@ package com.ll.tdd20240205.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,4 +20,9 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
+    }
 }
+
