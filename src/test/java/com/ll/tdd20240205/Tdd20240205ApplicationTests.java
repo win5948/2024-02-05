b9a6830460a459d37c08607baa481f6687e12ea5
win5948 <win5948@gmail.com>
2024-02-05 15:59:34 +0900

    Answerlist

--- src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
+++ src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,9 +21,13 @@ class Tdd20240205ApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2, a.getQuestion().getId());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
