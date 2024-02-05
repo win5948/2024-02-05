5a3898b4cf512576f0d78076b9913e0996a225c5
win5948 <win5948@gmail.com>
2024-02-05 15:51:42 +0900

    findBySubject

--- src/main/java/com/ll/tdd20240205/QuestionRepository.java
+++ src/main/java/com/ll/tdd20240205/QuestionRepository.java
@@ -3,5 +3,6 @@ package com.ll.tdd20240205;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
 
 }
--- src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
+++ src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
@@ -16,10 +16,7 @@ class Tdd20240205ApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if(oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 }
