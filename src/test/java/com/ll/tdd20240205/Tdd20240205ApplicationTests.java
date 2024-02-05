2fe57ea999424251916c6f5da152207ddaafcaf1
win5948 <win5948@gmail.com>
2024-02-05 16:00:22 +0900

    Transactional

--- src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
+++ src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
@@ -3,6 +3,8 @@ package com.ll.tdd20240205;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +21,7 @@ class Tdd20240205ApplicationTests {
     @Autowired
     private AnswerRepository answerRepository;
 
+    @Transactional
     @Test
     void testJpa() {
         Optional<Question> oq = this.questionRepository.findById(2);
