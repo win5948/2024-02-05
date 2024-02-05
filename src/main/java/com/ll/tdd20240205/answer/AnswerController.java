60a8bb59b43bb97057bd219224e060903c3483b5
win5948 <win5948@gmail.com>
2024-02-05 17:20:44 +0900

    Aservice

---
+++ src/main/java/com/ll/tdd20240205/answer/AnswerController.java
@@ -0,0 +1,27 @@
+package com.ll.tdd20240205.answer;
+
+import ch.qos.logback.core.model.Model;
+import com.ll.tdd20240205.question.Question;
+import com.ll.tdd20240205.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
