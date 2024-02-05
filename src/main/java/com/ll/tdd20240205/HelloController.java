dc76c72091448d057425460250bf116ae6316dd0
win5948 <win5948@gmail.com>
2024-02-05 14:26:35 +0900

    helloController

---
+++ src/main/java/com/ll/tdd20240205/HelloController.java
@@ -0,0 +1,14 @@
+package com.ll.tdd20240205;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}
