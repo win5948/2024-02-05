0c0a76debf3d20e202fd54aedf6076e318ad5404
win5948 <win5948@gmail.com>
2024-02-05 13:18:35 +0900

    start

---
+++ src/main/java/com/ll/tdd20240205/Tdd20240205Application.java
@@ -0,0 +1,13 @@
+package com.ll.tdd20240205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tdd20240205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tdd20240205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/tdd20240205/Tdd20240205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.tdd20240205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tdd20240205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
